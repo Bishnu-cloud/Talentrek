065070f65ebf6b66f06c1c3b35f7dcf7ca312269
Bishnu-cloud <your@gmail.com>
2026-04-18 18:26:34 +0530

    added postgres config

--- talentrek/settings.py
+++ talentrek/settings.py
@@ -1,6 +1,8 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -80,11 +82,11 @@ TEMPLATES = [
 WSGI_APPLICATION = 'talentrek.wsgi.application'
 
 # Database
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
 
 # Password validation
